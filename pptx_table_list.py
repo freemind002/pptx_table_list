c06ac331ac1521ba399149df9de035b4f1d6207a
freemind0825 <79529796+freemind0825@users.noreply.github.com>
2023-01-22 01:44:06 +0800

    Update pptx_table_list.py

--- pptx_table_list.py
+++ pptx_table_list.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 # 將事件編號、事件名稱、處理情形放入表格當中
 import pandas as pd
 from pptx import Presentation
@@ -12,7 +13,7 @@ from pptx.enum.text import MSO_ANCHOR
 n = int(input("請輸入？資料為一頁："))
 # 定義dataframe中的資料
 # 事件名稱
-case_name = ['事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', ]
+case_name = ['事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX', '事件名稱XXX']
 # ================================更改的參數=====================================
 # 事件編號，從case_name的索引取得
 case_number = [i+1 for i, j in enumerate(case_name)]
@@ -33,6 +34,19 @@ prs.slide_height, prs.slide_width = Cm(19.05), Cm(25.4)  # 設定ppt的高度 # 
 # 1.如果是10的倍數筆資料(如10、20、30...)，則插入 len(case_name) // 10 頁
 # 2.如果不是，則插入 len(case_name) // 10 + 1 頁
 blank_page = len(case_name) // n if len(case_name) % n == 0 else len(case_name) // n + 1
+
+
+def df_to_row(table, df, z, i, j):
+    cell = table.cell(i+1-n*z, j)
+    cell.vertical_anchor = MSO_ANCHOR.MIDDLE
+    tf = cell.text_frame
+    para = tf.paragraphs[0]
+    para.text = str(df.iloc[i, j])
+    para.font.size = Pt(16)
+    para.font.name = '微軟正黑體'
+    para.alignment = PP_ALIGN.CENTER  # 水平置中對齊
+
+
 for z in range(blank_page):
     # insert_blank_slide(6, Cm(1.76), Cm(0.49), Cm(21.89), Cm(1.28), "事件處理統計列表", '微軟正黑體', Pt(39))
     blank_slide_layout = prs.slide_layouts[6]  # 用內置模板(0-10)添加一個全空的ppt頁面
@@ -80,42 +94,19 @@ for z in range(blank_page):
     if len(case_name) % n == 0:
         for i in range(0+n*z, n+n*z):
             for j in range(c):
-                cell = table.cell(i+1-n*z, j)
-                cell.vertical_anchor = MSO_ANCHOR.MIDDLE
-                tf = cell.text_frame
-                para = tf.paragraphs[0]
-                para.text = str(df.iloc[i, j])
-                para.font.size = Pt(16)
-                para.font.name = '微軟正黑體'
-                para.alignment = PP_ALIGN.CENTER  # 水平置中對齊
+                df_to_row(table, df, z, i, j)
     # 如果資料不為10的倍數筆資料，處理方式
     else:
         # 最後一頁時，資料的處理方式
         if z+1 == blank_page:
             for i in range(0+n*z, len(case_name)):
                 for j in range(c):
-                    cell = table.cell(i+1-n*z, j)
-                    cell.vertical_anchor = MSO_ANCHOR.MIDDLE
-                    tf = cell.text_frame
-                    para = tf.paragraphs[0]
-                    para.text = str(df.iloc[i, j])
-                    para.font.size = Pt(16)
-                    para.font.name = '微軟正黑體'
-                    para.alignment = PP_ALIGN.CENTER
+                    df_to_row(table, df, z, i, j)
         else:
             # 前面的頁數時，與一般情況相同，因此處理方式相同
             for i in range(0+n*z, n+n*z):
                 for j in range(c):
-                    cell = table.cell(i+1-n*z, j)
-                    cell.vertical_anchor = MSO_ANCHOR.MIDDLE
-                    tf = cell.text_frame
-                    para = tf.paragraphs[0]
-                    # print(df.iloc[i,j])
-                    para.text = str(df.iloc[i, j])
-                    para.font.size = Pt(16)
-                    para.font.name = '微軟正黑體'
-                    para.alignment = PP_ALIGN.CENTER  # 水平置中對齊
-        # print(z)
+                    df_to_row(table, df, z, i, j)
 
 prs.save('pptx_table_list.pptx')
 print('PPTX製作完成')
